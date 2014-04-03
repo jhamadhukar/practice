e0b55402aedaa33e3b91090ac69626c75c4cb208
jhamadhukar <madhukar.jha@hotmail.com>
2014-04-03 18:16:11 +0530

    With Inheritence and Polymorphism

--- ConsoleApplication1/Inheritence.cs
+++ ConsoleApplication1/Inheritence.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Inheritence
+    public class Inheritence
     {
+        public static void Main(string[] args)
+        {
+            Stock stock = new Stock { Name = "hdfc", SharesOwned = 100 };
+            House house = new House { Name = "bac", Mortgage = 100};
+        }
+    }
+    public class Asset
+    {
+        public string Name;
+    }
+    public class Stock: Asset
+    {
+        public decimal SharesOwned;
+    }
+    public class House: Asset
+    {
+        public decimal Mortgage;
     }
 }
---
+++ ConsoleApplication1/Polymorphism.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Polymorphism
+    {
+        public static void Main(string[] args)
+        {
+            House house = new House { Name="house"};
+            Display(house);
+            Console.Read();
+        }
+        public static void Display(Asset asset)
+        {
+            Console.WriteLine(asset.Name);
+        }
+    }
+}
